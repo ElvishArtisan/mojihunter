5eb560de1364515eb9bd3a4a8b61bea9507dc3fc
Fred Gleason <fredg@paravelsystems.com>
2019-09-26 17:12:00 -0400

    2019-09-26 Fred Gleason <fredg@paravelsystems.com> 	* Added a 'fonthunter4' application.

---
+++ src/fonthunter/qt4/fonthunter4.cpp
@@ -0,0 +1,301 @@
+// fonthunter4.cpp
+//
+// A utility for testing fonts.
+//
+//   (C) Copyright 2019 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <qapplication.h>
+#include <qfontinfo.h>
+#include <qstringlist.h>
+//Added by qt3to4:
+#include <QResizeEvent>
+#include <QLabel>
+
+#include "fonthunter4.h"
+
+MainWidget::MainWidget(QWidget *parent)
+  : QWidget(parent)
+{
+  //
+  // Create Fonts
+  //
+  QFont label_font(font().family(),font().pointSize(),QFont::Bold);
+
+  //
+  // Set Window Title
+  //
+  setWindowTitle(tr("FontHunter - Qt3"));
+  setMinimumSize(sizeHint());
+
+  //
+  // Family
+  //
+  font_family_label=new QLabel(tr("Family")+":",this);
+  font_family_label->setFont(label_font);
+  font_family_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_family_edit=new QLineEdit(this);
+  font_family_edit->setText(font().family());
+
+  //
+  // Size
+  //
+  font_size_label=new QLabel(tr("Size")+":",this);
+  font_size_label->setFont(label_font);
+  font_size_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_size_spin=new QSpinBox(this);
+  font_size_spin->setRange(2,200);
+  font_size_spin->setValue(font().pointSize());
+
+  //
+  // Weight
+  //
+  font_weight_label=new QLabel(tr("Weight")+":",this);
+  font_weight_label->setFont(label_font);
+  font_weight_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_weight_box=new QComboBox(this);
+
+  font_weights.push_back(QFont::Light);
+  font_weight_names.push_back(tr("Light"));
+  font_weight_box->insertItem(font_weight_box->count(),tr("Light"));
+
+  font_weights.push_back(QFont::Normal);
+  font_weight_names.push_back(tr("Normal"));
+  font_weight_box->insertItem(font_weight_box->count(),tr("Normal"));
+
+  font_weights.push_back(QFont::DemiBold);
+  font_weight_names.push_back(tr("DemiBold"));
+  font_weight_box->insertItem(font_weight_box->count(),tr("DemiBold"));
+
+  font_weights.push_back(QFont::Bold);
+  font_weight_names.push_back(tr("Bold"));
+  font_weight_box->insertItem(font_weight_box->count(),tr("Bold"));
+
+  font_weights.push_back(QFont::Black);
+  font_weight_names.push_back(tr("Black"));
+  font_weight_box->insertItem(font_weight_box->count(),tr("Black"));
+
+  for(unsigned i=0;i<font_weights.size();i++) {
+    if(font_weights.at(i)>=font().weight()) {
+      font_weight_box->setCurrentIndex(i);
+      break;
+    }
+  }
+
+  //
+  // Italic
+  //
+  font_italic_label=new QLabel(tr("Italicize")+":",this);
+  font_italic_label->setFont(label_font);
+  font_italic_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_italic_box=new QComboBox(this);
+  font_italic_box->insertItem(0,tr("No"));
+  font_italic_box->insertItem(1,tr("Yes"));
+
+  //
+  // Apply Button
+  //
+  font_apply_button=new QPushButton(tr("Apply"),this);
+  font_apply_button->setFont(label_font);
+  connect(font_apply_button,SIGNAL(clicked()),this,SLOT(applyData()));
+
+  //
+  // Metric Value Outputs
+  //
+  font_metric_exact_match_label=new QLabel(tr("Exact Match")+":",this);
+  font_metric_exact_match_label->setFont(label_font);
+  font_metric_exact_match_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_metric_exact_match_value=new QLabel(this);
+  font_metric_exact_match_value->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+
+  font_metric_family_label=new QLabel(tr("Family")+":",this);
+  font_metric_family_label->setFont(label_font);
+  font_metric_family_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_metric_family_value=new QLabel(this);
+  font_metric_family_value->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+
+  font_metric_size_label=new QLabel(tr("Point Size")+":",this);
+  font_metric_size_label->setFont(label_font);
+  font_metric_size_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_metric_size_value=new QLabel(this);
+  font_metric_size_value->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+
+  font_metric_weight_label=new QLabel(tr("Weight")+":",this);
+  font_metric_weight_label->setFont(label_font);
+  font_metric_weight_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_metric_weight_value=new QLabel(this);
+  font_metric_weight_value->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+
+  font_metric_bold_label=new QLabel(tr("Is Bold")+":",this);
+  font_metric_bold_label->setFont(label_font);
+  font_metric_bold_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_metric_bold_value=new QLabel(this);
+  font_metric_bold_value->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+
+  font_metric_italic_label=new QLabel(tr("Is Italicized")+":",this);
+  font_metric_italic_label->setFont(label_font);
+  font_metric_italic_label->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+  font_metric_italic_value=new QLabel(this);
+  font_metric_italic_value->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+
+  //
+  // Samples
+  //
+  font_sample_label_label=new QLabel("QLabel",this);
+  font_sample_label_label->setFont(label_font);
+  font_sample_label_label->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+  font_sample_label=new QLabel(this);
+  font_sample_label->setFrameStyle(QFrame::Box|QFrame::Plain);
+  font_sample_label->setLineWidth(1);
+
+  font_sample_edit_label=new QLabel("QLineEdit",this);
+  font_sample_edit_label->setFont(label_font);
+  font_sample_edit_label->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+  font_sample_edit=new QLineEdit(this);
+  connect(font_sample_edit,SIGNAL(textChanged(const QString &)),
+	  font_sample_label,SLOT(setText(const QString &)));
+
+  font_sample_textedit_label=new QLabel("QTextEdit",this);
+  font_sample_textedit_label->setFont(label_font);
+  font_sample_textedit_label->setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+  font_sample_textedit=new QTextEdit(this);
+  font_sample_textedit->setAcceptRichText(true);
+
+  applyData();
+}
+
+
+QSize MainWidget::sizeHint() const
+{
+  return QSize(400,622);
+}
+
+
+void MainWidget::applyData()
+{
+  QFont test_font(font_family_edit->text(),font_size_spin->value(),
+		  font_weights.at(font_weight_box->currentIndex()),
+		  font_italic_box->currentIndex());
+  QFontInfo fi(test_font);
+
+  if(fi.exactMatch()) {
+    font_metric_exact_match_value->setText(tr("True"));
+  }
+  else {
+    font_metric_exact_match_value->setText(tr("False"));
+  }
+  font_metric_family_value->setText(fi.family());
+  font_metric_size_value->setText(QString().sprintf("%d",fi.pointSize()));
+  QString name="";
+  for(unsigned i=0;i<font_weights.size();i++) {
+    if(fi.weight()==font_weights.at(i)) {
+      name=font_weight_names[i];
+    }
+  }
+  if(name.isEmpty()) {
+    font_metric_weight_value->setText(QString().sprintf("%d",fi.weight()));
+  }
+  else {
+    font_metric_weight_value->setText(name);
+  }
+  if(fi.bold()) {
+    font_metric_bold_value->setText(tr("True"));
+  }
+  else {
+    font_metric_bold_value->setText(tr("False"));
+  }
+  if(fi.italic()) {
+    font_metric_italic_value->setText(tr("True"));
+  }
+  else {
+    font_metric_italic_value->setText(tr("False"));
+  }
+
+  font_sample_label->setFont(test_font);
+  font_sample_edit->setFont(test_font);
+  font_sample_textedit->setFont(test_font);
+
+  resizeEvent(NULL);
+}
+
+
+void MainWidget::resizeEvent(QResizeEvent *e)
+{
+  int w=width();
+
+  font_family_label->setGeometry(10,2,100,20);
+  font_family_edit->setGeometry(115,2,w-125,20);
+
+  font_size_label->setGeometry(10,27,100,20);
+  font_size_spin->setGeometry(115,27,w-125,20);
+
+  font_weight_label->setGeometry(10,52,100,20);
+  font_weight_box->setGeometry(115,52,w-125,20);
+
+  font_italic_label->setGeometry(10,77,100,20);
+  font_italic_box->setGeometry(115,77,60,20);
+
+  font_apply_button->setGeometry(10,102,w-20,40);
+
+  font_metric_exact_match_label->setGeometry(10,150,100,20);
+  font_metric_exact_match_value->setGeometry(115,150,w-20,20);
+
+  font_metric_family_label->setGeometry(10,175,100,20);
+  font_metric_family_value->setGeometry(115,175,w-20,20);
+
+  font_metric_size_label->setGeometry(10,200,100,20);
+  font_metric_size_value->setGeometry(115,200,w-20,20);
+
+  font_metric_weight_label->setGeometry(10,225,100,20);
+  font_metric_weight_value->setGeometry(115,225,w-20,20);
+
+  font_metric_bold_label->setGeometry(10,250,100,20);
+  font_metric_bold_value->setGeometry(115,250,w-20,20);
+
+  font_metric_italic_label->setGeometry(10,275,100,20);
+  font_metric_italic_value->setGeometry(115,275,w-20,20);
+
+  QFont test_font=font_sample_label->font();
+  int ypos=310;
+
+  font_sample_label_label->setGeometry(20,ypos,w-30,20);
+  ypos+=20;
+  font_sample_label->setGeometry(10,ypos,w-20,3*test_font.pointSize());
+  ypos+=3*test_font.pointSize()+20;
+
+  font_sample_edit_label->setGeometry(20,ypos,w-30,20);
+  ypos+=20;
+  font_sample_edit->setGeometry(10,ypos,w-20,3*test_font.pointSize());
+  ypos+=3*test_font.pointSize()+20;
+
+  font_sample_textedit_label->setGeometry(20,ypos,w-30,20);
+  ypos+=20;
+  font_sample_textedit->setGeometry(10,ypos,w-20,10*test_font.pointSize());
+  ypos+=10*test_font.pointSize()+20;
+}
+
+
+int main(int argc,char *argv[])
+{
+  QApplication a(argc,argv);
+
+  MainWidget *w=new MainWidget();
+  w->show();
+  return a.exec();
+}
---
+++ src/fonthunter/qt4/fonthunter4.h
@@ -0,0 +1,93 @@
+// fonthunter4.h
+//
+// A utility for testing fonts.
+//
+//   (C) Copyright 2019 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#ifndef FONTHUNTER4_H
+#define FONTHUNTER4_H
+
+#include <vector>
+
+#include <qcombobox.h>
+#include <qlabel.h>
+#include <qlineedit.h>
+#include <qpushbutton.h>
+#include <qsize.h>
+#include <qspinbox.h>
+#include <qtextedit.h>
+#include <qwidget.h>
+//Added by qt3to4:
+#include <QResizeEvent>
+
+class MainWidget : public QWidget
+{
+  Q_OBJECT
+ public:
+  MainWidget(QWidget *parent=0);
+  QSize sizeHint() const;
+
+ private slots:
+  void applyData();
+
+ protected:
+  void resizeEvent(QResizeEvent *e);
+
+ private:
+  QLabel *font_family_label;
+  QLineEdit *font_family_edit;
+  QLabel *font_size_label;
+  QSpinBox *font_size_spin;
+  QLabel *font_weight_label;
+  QComboBox *font_weight_box;
+  std::vector<QFont::Weight> font_weights;
+  QStringList font_weight_names;
+  QLabel *font_italic_label;
+  QComboBox *font_italic_box;
+
+
+  QPushButton *font_apply_button;
+
+  QLabel *font_metric_exact_match_label;
+  QLabel *font_metric_exact_match_value;
+
+  QLabel *font_metric_family_label;
+  QLabel *font_metric_family_value;
+
+  QLabel *font_metric_size_label;
+  QLabel *font_metric_size_value;
+
+  QLabel *font_metric_weight_label;
+  QLabel *font_metric_weight_value;
+
+  QLabel *font_metric_bold_label;
+  QLabel *font_metric_bold_value;
+
+  QLabel *font_metric_italic_label;
+  QLabel *font_metric_italic_value;
+
+
+  QLabel *font_sample_label_label;
+  QLabel *font_sample_label;
+  QLabel *font_sample_edit_label;
+  QLineEdit *font_sample_edit;
+  QLabel *font_sample_textedit_label;
+  QTextEdit *font_sample_textedit;
+};
+
+
+#endif  // FONTHUNTER4_H
